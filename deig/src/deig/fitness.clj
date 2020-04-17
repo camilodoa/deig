3b05cac044e127196bbe4152385507e1871022eb
SergeiBond <62620821+SergeiBond@users.noreply.github.com>
2020-04-17 15:57:27 -0400

    my fitness function  (mostly done but still needs work)

---
+++ deig/src/deig/fitness.clj
@@ -0,0 +1,23 @@
+(ns deig.fitness
+    (:require [libpython-clj.python :as py]))
+
+(require-python
+  '[numpy :as np]
+  '[keras_preprocessing :as image-processing]
+  '[tensorflow.keras.models :as models])
+
+
+
+(defn fitness [img]
+    (let [x (image-processing.image/img_to_array img)
+          x (np/expand_dims x 0)
+          model (models/load_model "model.h5" false)
+          probabilities (first (model.predict_proba x))
+          fitness (* (apply max probabilities) 100)]
+         fitness))cd
+
+
+
+
+
+
