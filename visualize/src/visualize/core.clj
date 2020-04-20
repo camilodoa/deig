74232da9db135335a97b0c7bc0cdbc1eca2a2dba
cshim20 <62620422+cshim20@users.noreply.github.com>
2020-04-20 12:54:32 -0400

    works now

--- visualize/src/visualize/core.clj
+++ visualize/src/visualize/core.clj
@@ -1,47 +1,45 @@
 (ns visualize.core
-  (:require [quil.core :as q :include-macros true]          ;; add this
-            [quil.middleware :as middleware]))
 
-(def viz-width 28)                                          ;; width of the visualization window in pixels
-(def viz-height 28)                                         ;; height of the visualization window in pixels
-(def num_pixels 200)
+      (:require [quil.core :as q :include-macros true]          ;; add this
+                [quil.middleware :as middleware]))
 
-(defn grayscale-pixel [] (rand-int 255))
+  (def viz-width 28)                                          ;; width of the visualization window in pixels
+  (def viz-height 28)                                         ;; height of the visualization window in pixels
 
-(defn create-grayscale-pixels [dimension]
-  (vec (repeatedly dimension
-                   #(vec (repeatedly dimension grayscale-pixel)))))
+  (def viz-width_div_2 14)                                          ;; width of the visualization window in pixels
+  (def viz-height_div_2 14)
 
-;(def n (create-grayscale-pixels viz-width))
-;(print n)
-;(print (get (get n 1) 1))
+  (defn grayscale-pixel [] (rand-int 255))
 
-(defn visualize_pixels []
-  ;; set background color to white 255
-  (let [n (create-grayscale-pixels viz-width)
-        gr (q/background 255)
-        im (q/create-image (* 2 viz-width) (* 2 viz-height) :rgb)]
+  (defn create-grayscale-pixels [dimension]
+        (vec (repeatedly dimension
+                         #(vec (repeatedly dimension grayscale-pixel)))))
 
-    ;; randomly set this many pixels
-    (doseq [i (range viz-width)
-            j (range viz-width)]
+  (defn visualize_pixels []
+    ;; set background color to white 255
+        (let [n (create-grayscale-pixels viz-width)
+              gr (q/background 255)
+              im (q/create-image viz-width viz-height :rgb)]
 
-      (q/color-mode :hsb)
+          ;; randomly set this many pixels
+          (doseq [i (range viz-width)
+                  j (range viz-height)]
 
-      (let [b (get (get n i) j)]
-        (q/set-pixel im i j
-                     (q/color 0 0 b))))
-    (q/set-image 0 0 im))
-  (q/no-loop))
+            (q/color-mode :hsb)
 
-(defn start-visualization []
-  "Create the Quil sketch."
-  (q/sketch
-    :size [viz-width viz-height]
-    :draw visualize_pixels))
+            (let [b (get (get n i) j)]
+              (q/set-pixel im i j
+                           (q/color 0 0 b))))
+          (q/set-image 0 0 im))
+        (q/no-loop))
+
+  (defn start-visualization []
+        "Create the Quil sketch."
+        (q/sketch
+          :size [viz-width_div_2 viz-height_div_2]
+          :draw visualize_pixels))
 
 
-(start-visualization)
-;;;
-
+  (start-visualization)
+  ;;;
 
